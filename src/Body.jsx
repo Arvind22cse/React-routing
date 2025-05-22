51704a6d2b694d806660f7f49b9997286e3445cf
Arvind22cse <arvindm.22cse@kongu.edu>
2025-05-22 02:19:33 +0000

    dev

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,7 +3,7 @@ function Body() {
     return (
       <>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand">About</a>
+  <a class="navbar-brand">About me</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
